70d96793b0c3ba0af59118fc290a2074608052f9
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2017-11-04 14:44:23 -0200

    binding

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
-	render() {
-		return <input onChange={this.onInputChange} />
+
+	constructor(props){
+		super(props);
+		this.state = { term: '' };
+
+		this.onInputChange = this.onInputChange.bind(this);
 	}
 
-	onInputChange(ev){
-		console.log(event.target.value);
+	render() {
+		return (
+			<div>
+				<input 
+					value={this.state.term}
+					onChange={this.onInputChange} />
+				<div><small>Value {this.state.term} </small></div>
+			</div>
+		)
+	}
+	
+	onInputChange(ev) {
+		this.setState({ term: ev.target.value });
 	}
 }
 
