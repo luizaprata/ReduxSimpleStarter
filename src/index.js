b28c9cdd93b613ce679c0c393a3750d04c9180b1
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2017-11-04 15:29:56 -0200

    yt search test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD0HfYOwSm9Ig4lukCSv_z_Bz7QRp3Y9rA';
+
+YTSearch({key: API_KEY, term:'surfboards'}, function(data){
+	console.log(data);
+})
+
 // Create new component. this should produce some HTML
 const App = () => {
 	return (
