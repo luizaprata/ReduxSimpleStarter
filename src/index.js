38540f8dd4fb82acdb1bd6798627831f4c17e639
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2017-11-15 16:59:07 -0200

    video list

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD0HfYOwSm9Ig4lukCSv_z_Bz7QRp3Y9rA';
 
-YTSearch({key: API_KEY, term:'surfboards'}, function(data){
-	console.log(data);
-})
+
 
 // Create new component. this should produce some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor (props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term:'surfboards'},(videos) => {
+			this.setState({ videos });
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 //take this component's generated HTML and put it in the DOM
 ReactDOM.render(<App/>, document.querySelector('.container'));
