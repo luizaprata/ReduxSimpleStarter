52282c589b47b68df1448eedc9eaf3d68e3e9fab
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2017-11-16 18:35:06 -0200

    [VideoDetails] add

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyD0HfYOwSm9Ig4lukCSv_z_Bz7QRp3Y9rA';
 
@@ -25,6 +26,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
