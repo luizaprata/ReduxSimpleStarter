1df5bd394a2bf2d03c8ac96e8815b6cf6cca9c4a
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2017-11-16 17:39:36 -0200

    [video] ajustando html lista videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+
+	const imageURL = video.snippet.thumbnails.default.url
+	const imageTitle = video.snippet.title
 	return (
-		<li>{JSON.stringify(props.video)}</li>
+		<li className="list-group-item">
+			<div className="video-list media">
+				<img src="" className="media-object" src={imageURL}/>
+			</div>
+			<div className="media-body">
+				<div className="media-heading">{imageTitle}</div>
+			</div>
+		</li>
 	)
 }
 
